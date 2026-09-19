fe2afd03b5143335587741e726dc796c846ab756
jarojas2026 <jarojas800@gmail.com>
2026-09-18 23:26:35 -0600

    Remove obsolete orchestration barrel

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import { api } from './apiManager';
-
-// Motor de comunicaciones nativo: API interna + Firestore + servicios de IA.
-console.log('[Native Core] API Manager listo.');
-
-export { api };
