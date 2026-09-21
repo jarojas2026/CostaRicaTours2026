5f16c6ef74ef56d83cde8de2bfea6ada8d079dc4
jarojas2026 <jarojas800@gmail.com>
2026-09-20 22:21:15 -0600

    refactor: remove n8n references from core configuration

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,3 @@
-
 import { api } from './apiManager';
-import * as triggers from './n8nTriggers';
 
-// Log loaded
-console.log('n8n Triggers Ready:', Object.keys(triggers).length, 'triggers available.');
+export { api };
